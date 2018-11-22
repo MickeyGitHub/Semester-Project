69eee6b8e5e5769f91862abba0cbb716552d9ea7
zherbz <44210203+zherbz@users.noreply.github.com>
2018-11-22 11:12:06 -0700

    function is split in half
    Need to figure out if its possible to render multiple things (images, plots, tables, etc) by calling the function once. Otherwise, we'll have to either call it multiple times which will really take a long time or split the function up like in this version of the app.

--- app.R
+++ app.R
@@ -31,7 +31,6 @@ crops <- read.csv('data/crops.csv',header=TRUE)
 OptimalStation <- function(lat, long){
   start_date <- '2017-01-01'
   end_date <- '2018-01-01'
-  # Start of 1st function
   lat_lon_df <- data.frame(id = "Station", latitude = lat, longitude = long)
   #Load in all station metadata from previously saved file
   options(noaakey = "YzLzNDLCXIzUwfAsWljYgxvxmZPMHtIj")
@@ -102,6 +101,67 @@ OptimalStation <- function(lat, long){
   ggsave("NearbyWeatherStations.png", width = 7, height = 5)
   pathname <- '/Users/Zach/Documents/Hydroinformatics 7460/AguaLibre'
   localMap <- paste0(pathname,'/NearbyWeatherStations.png')
+  output_data <- localMap
+  #End
+  return(output_data)
+}
+
+OptimalData <- function(lat, long){
+  start_date <- '2017-01-01'
+  end_date <- '2018-01-01'
+  # Start of 1st function
+  lat_lon_df <- data.frame(id = "Station", latitude = lat, longitude = long)
+  #Load in all station metadata from previously saved file
+  options(noaakey = "YzLzNDLCXIzUwfAsWljYgxvxmZPMHtIj")
+  library(rnoaa)
+  load("/Users/Zach/Documents/Hydroinformatics 7460/AguaLibre/station_data.Rdata") 
+  #Retrieve the 10 closest weather station's metadata (distance in km)
+  closest_stations <- meteo_nearby_stations(lat_lon_df = lat_lon_df,
+                                            station_data = station_data,
+                                            radius = 10)
+  closest_stations <- as.data.frame(closest_stations)
+  lat <- as.vector(closest_stations$Station.latitude)
+  long <- as.vector(closest_stations$Station.longitude)
+  #Retrieve data from closest weather stations (prcp in tenths of mm, temp in tenths of decC)
+  monitorIDs <- as.vector(closest_stations[[1]])
+  climate_data <- meteo_pull_monitors(monitorIDs,
+                                      var = c('PRCP'),
+                                      date_min = start_date,
+                                      date_max = end_date)
+  #Determine which stations have the most and least amount of available data
+  station_data <- vector(mode = "list", length = nrow(closest_stations))
+  for (i in 1:nrow(closest_stations)){
+    df <- climate_data[climate_data$id == closest_stations[i,1],]
+    id <- df$id[1]
+    avail_prcp <- nrow(df)
+    if (avail_prcp == 0){
+      avail_prcp <- NA
+      id <- closest_stations[i,1]
+    }
+    v <- c(id, avail_prcp)
+    station_data[[i]] <- v
+  }
+  station_data <-  do.call(rbind, station_data)
+  colnames(station_data) <- c('id', 'avail_prcp')
+  #Populate the closest stations data with this new information
+  newcols <- c('AvailableData')
+  closest_stations[, newcols] <- NA
+  for (j in 1:nrow(closest_stations)){
+    closest_stations$AvailableData[j] <- as.numeric(station_data[j,2])
+  }
+  #Determine optimal station based on distance from user location and available data
+  closest_stations <- closest_stations[!is.na(closest_stations$AvailableData),]
+  dist <- rank(closest_stations$Station.distance)
+  dat <- rank(-closest_stations$AvailableData)
+  tot <- dist + dat
+  rankings <- cbind(closest_stations$Station.id, closest_stations$Station.name, dist, dat, tot)
+  colnames(rankings) <- c('id', 'Name', 'Distance','AvailableData', 'Total')
+  row_pos <- which.min(rankings[,5])
+  optimal_station <- rankings[row_pos,1]
+  optimal_data <- climate_data[climate_data$id == optimal_station,]
+  optimal_data$SiteName <- id
+  station_metadata <- closest_stations[which(closest_stations$Station.id == optimal_station),]
+  prcp_data <- optimal_data
   # End of 1st function
   # Start of 2nd function
   # Load new packages 
@@ -111,7 +171,7 @@ OptimalStation <- function(lat, long){
   # Input
   lat <- station_metadata[3]
   long <- station_metadata[4]
-  start_date <- '2000-01-01'
+  start_date <- '1980-01-01'
   # Save base and full url as variables
   url_usu = "https://climate.usu.edu/API/api.php/v2/key=62PyzUjrCDYh0JB95faxrDcGB9tTss/evapotranspiration/average_monthly_sum"
   full_url = paste0(url_usu,'/state=UT/network=ghcn','/lat=',lat,'/long=',long,'/start_date=',start_date,'/end_date=',end_date,'/units=m/month=(1,2,3,4,5,6,7,8,9,10,11,12)/buffer=10')
@@ -125,8 +185,7 @@ OptimalStation <- function(lat, long){
   std <- as.numeric(evap_data[,3])
   evap_data <- data.frame('month' = mon, 'evap_sum' = evap, 'std_dev' = std)
   # End of 2nd function
-  output_data <- list(prcp_data, evap_data, localMap)
-  #End
+  output_data <- list(prcp_data, evap_data)
   return(output_data)
 }
 ##ENDglobal##ENDglobal##ENDglobal##ENDglobal##ENDglobal##ENDglobal##ENDglobal##ENDglobal##ENDglobal##ENDglobal
@@ -209,7 +268,7 @@ ui <- fluidPage(
     mainPanel(
     )
   )
-)
+  )
 
 ######ENDui######ENDui######ENDui######ENDui######ENDui######ENDui######ENDui######ENDui######ENDui######
 
@@ -227,12 +286,17 @@ server <- function(input, output, session) {
       setView(lng = input$Long, lat = input$Lat, zoom = 16 )  %>%
       addMeasure()
   })
-
+  
   # Render map image showing user location relative to nearby weather stations
   output$map2 <- renderImage({
     data1 <- OptimalStation(lat = input$Lat, long = input$Long)
-    source <- data1[[3]]
-    list(src = source, contentType = 'image/png', width = 700, height = 600)
+    source <- data1
+    list(src = source, contentType = 'image/png', width = 500, height = 400)
+  })
+  
+  output$table1 <- renderTable({
+    data2 <- OptimalData(lat = input$Lat, long = input$Long)
+    evap_data <- data2[[2]]
   })
   
 }
