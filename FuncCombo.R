f34eb75389ad15d22265751ff3efdd9aa8cf84e0
zherbz <44210203+zherbz@users.noreply.github.com>
2018-11-23 15:19:59 -0700

    updates

--- FuncCombo.R
+++ FuncCombo.R
@@ -1,18 +1,20 @@
-OptimalStation <- function(lat, long, start_date, end_date){
+OptimalStation <- function(lat, long){
+  start_date <- '2017-01-01'
+  end_date <- '2018-01-01'
   # Start of 1st function
   lat_lon_df <- data.frame(id = "Station", latitude = lat, longitude = long)
   #Load in all station metadata from previously saved file
   options(noaakey = "YzLzNDLCXIzUwfAsWljYgxvxmZPMHtIj")
   library(rnoaa)
   load("/Users/Zach/Documents/Hydroinformatics 7460/AguaLibre/station_data.Rdata") 
-  #Retrieve the 10 closest weather station's metadata (distance in km)
+  #Retrieve the closest weather station's metadata within a 10 km radius
   closest_stations <- meteo_nearby_stations(lat_lon_df = lat_lon_df,
                                             station_data = station_data,
                                             radius = 10)
   closest_stations <- as.data.frame(closest_stations)
   lat <- as.vector(closest_stations$Station.latitude)
   long <- as.vector(closest_stations$Station.longitude)
-  #Retrieve data from closest weather stations (prcp in tenths of mm, temp in tenths of decC)
+  #Retrieve data from closest weather stations (prcp in tenths of mm)
   monitorIDs <- as.vector(closest_stations[[1]])
   climate_data <- meteo_pull_monitors(monitorIDs,
                                       var = c('PRCP'),
@@ -53,6 +55,26 @@ OptimalStation <- function(lat, long, start_date, end_date){
   station_metadata <- closest_stations[which(closest_stations$Station.id == optimal_station),]
   prcp_data <- optimal_data
   rm(i,j,id,newcols,avail_prcp,dat,dist,tot,v,row_pos,station_data,df)
+  # Determine monthly precp 
+  values <- prcp_data$prcp
+  dates <- prcp_data$date
+  dates <- as.Date.factor(dates)
+  dates <- format.Date(dates, "%m")
+  dates <- as.numeric(dates)
+  months <- vector(mode = "list", length = 12)
+  for (i in 1:12){
+    a <- months[[i]]
+    for (k in 1:length(values)){
+      if (dates[k] == i){
+        a[k] <- values[k]
+      }
+    }
+    months[[i]] <- a
+    months[[i]] <- sum(months[[i]], na.rm = TRUE)
+  }
+  monthly_prcp <-  do.call(rbind, months)
+  #Convert from tenths of mm to inches
+  monthly_prcp <- ((monthly_prcp/10)/1000)*39.3701
   #Create map showing user location relative to nearby weather stations
   library(ggmap)
   bbox <- make_bbox(long,lat,f=0.05)
@@ -77,11 +99,10 @@ OptimalStation <- function(lat, long, start_date, end_date){
   # Input
   lat <- station_metadata[3]
   long <- station_metadata[4]
-  start_date <- '1980-01-01'
-  end_date <- '2018-01-01'
+  start_date <- '2000-01-01'
   # Save base and full url as variables
   url_usu = "https://climate.usu.edu/API/api.php/v2/key=62PyzUjrCDYh0JB95faxrDcGB9tTss/evapotranspiration/average_monthly_sum"
-  full_url = paste0(url_usu,'/state=UT/network=ghcn','/lat=',lat,'/long=',long,'/start_date=',start_date,'/end_date=',end_date,'/units=m/month=(1,2,3,4,5,6,7,8,9,10,11,12)/buffer=10')
+  full_url = paste0(url_usu,'/state=UT/network=ghcn','/lat=',lat,'/long=',long,'/start_date=',start_date,'/end_date=',end_date,'/units=e/month=(1,2,3,4,5,6,7,8,9,10,11,12)/buffer=10')
   # Convert JSON to data frame
   evap_data <- fromJSON(getURL(full_url))
   # Break down list structure into numeric dataframe
@@ -90,10 +111,8 @@ OptimalStation <- function(lat, long, start_date, end_date){
   evap <- as.numeric(evap_data[,1])
   mon <- as.numeric(evap_data[,2])
   std <- as.numeric(evap_data[,3])
-  evap_data <- data.frame('month' = mon, 'evap_sum' = evap, 'std_dev' = std)
-  # End of 2nd function
-  output_data <- list(prcp_data, evap_data, localMap)
+  # Combine monthly prcp and evap data together in dataframe
+  climate_data <- data.frame('month' = mon, 'prcp_in' = monthly_prcp, 'evap_sum_in' = evap, 'evap_std' = std)
+  output_data <- list(climate_data, localMap)
   return(output_data)
 }
-
-
