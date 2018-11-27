e9c493a018e536df829bbc277e266d1b217132e2
zherbz <44210203+zherbz@users.noreply.github.com>
2018-11-27 14:38:11 -0700

    working directory

---
+++ AguaLibre2.0/PublishingIssue.R
@@ -0,0 +1,7 @@
+'https://stackoverflow.com/questions/46692240/shiny-app-that-fetches-data-via-url-works-locally-but-not-on-shinyapps-io'
+
+'https://github.com/jeroen/curl/issues/122'
+
+'https://support.rstudio.com/hc/en-us/articles/205002917-SSL-certificate-problem-when-publishing-to-RPubs'
+
+'https://support.rstudio.com/hc/en-us/articles/229848967-Why-does-my-app-work-locally-but-not-on-shinyapps-io-'
